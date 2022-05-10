616b2eafa2ac8078768b10d873e404ae780f5d80
Jacqueline Cirne Cuetlach <jacqueccprog@gmail.com>
2022-05-10 12:39:43 -0500

    Actualizacion

--- scripts/api.js
+++ scripts/api.js
@@ -1,13 +1,13 @@
 const contenedor = document.getElementById("contenido-principal");
 const url = "https://raw.githubusercontent.com/JacqueCiCu/Curso-FPW-API/master/base_datos.json";
-const uri ="https://github.com/JacqueCiCu/Curso-FPW-API/blob/master" 
+const uri ="https://raw.githubusercontent.com/JacqueCiCu/Curso-FPW-API/master/" 
 
 fetch(url).then(function(respuesta) {
     return respuesta.json();
 }).then(function(datos) {
     console.log(datos);
     for(let i = 0; i <datos.length; i++) {
-        contenedor.innerHTML += "div class='portadas'><img src='" +uri + datos[i]["imagen"] + "'><h3>" + datos[i]["imagen"] + "'></h3>"
+        contenedor.innerHTML += "<div class='card' class='face back'><img src='" +uri + datos[i]["imagen"] + "'><h3>" + datos[i]["nombre"] + datos[i]["texto"] + datos[i]["sinopsis"] + "</h3>"
     }
 }).catch(function(error) { 
     console.log(error);
