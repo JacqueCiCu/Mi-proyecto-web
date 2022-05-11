c1f7db1cc6ff18487a093245a522b98908df34c3
Jacqueline Cirne Cuetlach <jacqueccprog@gmail.com>
2022-05-11 01:18:21 -0500

    Actualizo

--- scripts/api.js
+++ scripts/api.js
@@ -2,12 +2,12 @@ const contenedor = document.getElementById("contenido-principal");
 const url = "https://raw.githubusercontent.com/JacqueCiCu/Curso-FPW-API/master/base_datos.json";
 const uri ="https://raw.githubusercontent.com/JacqueCiCu/Curso-FPW-API/master/" 
 
-fetch(url).then(function(respuesta) {
-    return respuesta.json();
-}).then(function(datos) {
-    console.log(datos);
-    for(let i = 0; i <datos.length; i++) {
-        contenedor.innerHTML += "<div class='card' class='face back'><img src='" +uri + datos[i]["imagen"] + "'><h3>" + datos[i]["nombre"] + datos[i]["texto"] + datos[i]["sinopsis"] + "</h3>"
+fetch(url).then(function(response) {
+    return response.json();
+}).then(function(data) {
+    console.log(data);
+    for(let i = 0; i <data.length; i++) {
+        contenedor.innerHTML += "<div class='card' class='face  back'><img src='" +uri + data[i]["imagen"] + "'><h3>" + data[i]["nombre"] + data[i]["texto"] + data[i]["sinopsis"] + "</h3>"
     }
 }).catch(function(error) { 
     console.log(error);
